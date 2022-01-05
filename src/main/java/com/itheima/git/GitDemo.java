a7766d1d51c83c19a3465a15240ad99038d00873
rongshan <you@example.com>
2022-01-05 21:45:30 +0800

    第二次提交

--- src/main/java/com/itheima/git/GitDemo.java
+++ src/main/java/com/itheima/git/GitDemo.java
@@ -3,6 +3,7 @@ package com.itheima.git;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
     }
 
 }
